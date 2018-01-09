309a6cf6c90f8b14ab94d3af6e6298085c15a1b8
panatda inthala and tyler kostelak <panatda@>
2018-01-09 15:00:11 -0800

    wrote javascript that calculates BMI out of weight and height of user.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var bodyMassIndex = function(height, weight){
+	alert("your BMI is: " + (bmiEquation));
+};
+
+
+var height = parseInt(prompt("What is your height?"));
+
+var weight = parseInt(prompt("What is your weight?"));
+
+var bmiEquation = weight / height / height * 703;
+
+bodyMassIndex(bmiEquation)
